b9f89c29caf6bcebccd96c5e4f3cc692ca48220e
NataliyaBo <n1talika@yandex.ru>
2020-09-21 18:38:24 +0300

    8080

--- src/main/java/com/example/tml_mustache/controller/CarouselController.java
+++ src/main/java/com/example/tml_mustache/controller/CarouselController.java
@@ -8,19 +8,47 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import com.example.tml_mustache.entity.ItemWithNum;
+
 @Controller
 public class CarouselController {
 
     @GetMapping("/carousel")
     public ModelAndView workTimeList(Map<String, Object> model) {
 
-        List<String> carouselList = new ArrayList<>();
-        carouselList.add("img/shmel12a.jpg");
-        carouselList.add("img/shmel1a.jpg");
-        carouselList.add("img/shmel3a.jpg");
-        carouselList.add("img/shmel6a.jpg");
-        carouselList.add("img/shmel9a.jpg");
-        carouselList.add("img/shmel10a.jpg");
+        List<ItemWithNum> carouselList = new ArrayList<>();
+
+        ItemWithNum itemWithNum = new ItemWithNum();
+        itemWithNum.setIsFirst("y");
+        itemWithNum.setName("img/shmel12a.jpg");
+        itemWithNum.setIdx(0);
+        carouselList.add(itemWithNum);
+
+        itemWithNum = new ItemWithNum();
+        itemWithNum.setName("img/shmel1a.jpg");
+        itemWithNum.setIdx(1);
+        carouselList.add(itemWithNum);
+
+        itemWithNum = new ItemWithNum();
+        itemWithNum.setName("img/shmel3a.jpg");
+        itemWithNum.setIdx(2);
+        carouselList.add(itemWithNum);
+
+        itemWithNum = new ItemWithNum();
+        itemWithNum.setName("img/shmel6a.jpg");
+        itemWithNum.setIdx(3);
+        carouselList.add(itemWithNum);
+
+        itemWithNum = new ItemWithNum();
+        itemWithNum.setName("img/shmel9a.jpg");
+        itemWithNum.setIdx(4);
+        carouselList.add(itemWithNum);
+
+        itemWithNum = new ItemWithNum();
+        itemWithNum.setName("img/shmel10a.jpg");
+        itemWithNum.setIdx(5);
+        carouselList.add(itemWithNum);
+
         model.put("carouselList", carouselList);
 
         return new ModelAndView("carousel", model);
